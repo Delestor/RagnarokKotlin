7c3ff8d6de59be9714cb1519e27b27cfb4037cbe
Albert Cadena <albert.cad.rub@gmail.com>
2025-08-14 11:28:47 +0200

    First commit

---
+++ core/src/main/kotlin/com/cadena/ragnarok/Main.kt
@@ -0,0 +1,6 @@
+package com.cadena.ragnarok
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
+class Main : ApplicationAdapter()
