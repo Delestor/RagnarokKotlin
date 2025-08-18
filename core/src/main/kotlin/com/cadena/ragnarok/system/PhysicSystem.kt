16c6264227b83983ec61e7653aea26d3ea083add
Albert Cadena <albert.cad.rub@gmail.com>
2025-08-18 23:20:00 +0200

    Frame Interpolation

--- core/src/main/kotlin/com/cadena/ragnarok/system/PhysicSystem.kt
+++ core/src/main/kotlin/com/cadena/ragnarok/system/PhysicSystem.kt
@@ -1,5 +1,6 @@
 package com.cadena.ragnarok.system
 
+import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.physics.box2d.World
 import com.cadena.ragnarok.component.ImageComponent
 import com.cadena.ragnarok.component.PhysicComponent
@@ -13,11 +14,11 @@ class PhysicSystem(
     private val phWorld: World,
     private val imageCmps: ComponentMapper<ImageComponent>,
     private val physicCmps: ComponentMapper<PhysicComponent>
-) : IteratingSystem(interval = Fixed(1/60f)) {
+) : IteratingSystem(interval = Fixed(1 / 60f)) {
 
     override fun onUpdate() {
-        if(phWorld.autoClearForces){
-            log.error{ "AutoClearForces must be set to false to guarantee a correct physic simulation."}
+        if (phWorld.autoClearForces) {
+            log.error { "AutoClearForces must be set to false to guarantee a correct physic simulation." }
             phWorld.autoClearForces = false
         }
         super.onUpdate()
@@ -31,20 +32,32 @@ class PhysicSystem(
 
     override fun onTickEntity(entity: Entity) {
         val physicCmp = physicCmps[entity]
-        val imageCmp = imageCmps[entity]
 
-        if(!physicCmp.impulse.isZero){
+        physicCmp.prevPos.set(physicCmp.body.position)
+
+        if (!physicCmp.impulse.isZero) {
             physicCmp.body.applyLinearImpulse(physicCmp.impulse, physicCmp.body.worldCenter, true)
             physicCmp.impulse.setZero()
         }
 
+
+    }
+
+    override fun onAlphaEntity(entity: Entity, alpha: Float) {
+        val physicCmp = physicCmps[entity]
+        val imageCmp = imageCmps[entity]
+
+        val (prevX, prevY) = physicCmp.prevPos
         val (bodyX, bodyY) = physicCmp.body.position
         imageCmp.image.run {
-            setPosition(bodyX-width*0.5f, bodyY-height*0.5f)
+            setPosition(
+                MathUtils.lerp(prevX, bodyX, alpha) - width * 0.5f,
+                MathUtils.lerp(prevY, bodyY, alpha) - height * 0.5f
+            )
         }
     }
 
-    companion object{
+    companion object {
         private val log = logger<PhysicSystem>()
     }
 
