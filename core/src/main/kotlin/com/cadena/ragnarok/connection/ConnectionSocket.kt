0b94e1bb76fcce43cd7f0514dc17d0897ca42f39
Albert Cadena <albert.cad.rub@gmail.com>
2025-08-20 23:42:28 +0200

    Probando simular múltiples clientes

---
+++ core/src/main/kotlin/com/cadena/ragnarok/connection/ConnectionSocket.kt
@@ -0,0 +1,43 @@
+package com.cadena.ragnarok.connection
+
+import com.badlogic.gdx.net.SocketHints
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.Socket
+import kotlin.random.Random
+import kotlin.random.nextLong
+
+class ConnectionSocket : Socket() {
+
+    fun sendMessageToServer(){
+        val hints = SocketHints()
+        //hints.connectTimeout = 4000
+        //val client = Gdx.net.newClientSocket(Net.Protocol.TCP , "localhost", 9999, hints);
+        val client = Socket("localhost", 9999)
+
+        val waitTime:Long = Random.nextLong(0,100)
+
+        client.sendMessage("Hola Mundo")
+        Thread.sleep(waitTime)
+        client.sendMessage("Mensaje 2")
+        Thread.sleep(waitTime)
+        client.sendMessage("Mensaje 3")
+        Thread.sleep(waitTime)
+        client.sendMessage("Mensaje 4")
+        Thread.sleep(waitTime)
+        client.sendMessage("Mensaje 5")
+
+        client.close()
+    }
+
+    private fun Socket.sendMessage(mensaje:String) {
+        val output = PrintWriter(this.outputStream, true)
+        val input = BufferedReader(InputStreamReader(this.inputStream))
+
+        println("El cliente envia: [$mensaje]")
+        output.println(mensaje)
+
+        println("El cliente recibe: [${input.readLine()}]")
+    }
+}
